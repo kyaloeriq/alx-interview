60104102c252306e4acc26239a9ae025ef2defe9
kyaloeriq <kyaloeriq@gmail.com>
2024-08-02 23:30:36 -0700

    Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,6 +4,7 @@
 Module provides a function to determine if all boxes can be opened.
 """
 
+
 def canUnlockAll(boxes):
     """
     Determine if all the boxes can be opened.
@@ -12,12 +13,13 @@ def canUnlockAll(boxes):
     unlocked = [False] * n
     unlocked[0] = True
     keys = [0]
-    
+
     while keys:
         current_key = keys.pop()
         for key in boxes[current_key]:
             if key < n and not unlocked[key]:
                 unlocked[key] = True
                 keys.append(key)
-    
-    return all(unlocked)
+
+
+return all(unlocked)

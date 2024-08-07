a036e16a87b4dedbbaa3b99b98f72de33151dd90
kyaloeriq <kyaloeriq@gmail.com>
2024-08-07 12:15:12 -0700

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
-
 """
 Minimum Operations Module
 """
+
+
 def minOperations(n: int) -> int:
     """
     Calculate the fewest number of operations needed
     """
     if n <= 1:
         return 0
-    
     operations = 0
     factor = 2
-    
     while n > 1:
         while n % factor == 0:
             operations += factor
             n //= factor
         factor += 1
-        
     return operations
